4bf3078b2588bdd4e67360616d209763c4b975e9
Andre2130 <atrobinson30@gmail.com>
2018-05-14 13:20:54 -0400

    Initial commit first Android app in Kotlin

---
+++ app/src/main/java/com/example/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.samodelkin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCharacterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+    }
+}
